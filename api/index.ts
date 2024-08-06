2fbf5153d2a0e808f7e97230995872f5fda472a4
schrobby <schrawby@gmail.com>
2024-08-06 03:14:14 +0200

    Add henrik valorant api

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import 'unofficial-valorant-api';
+
+const vapi = new HenrikDevValorantAPI(process.env.API_KEY);
 
 export default function (request: VercelRequest, response: VercelResponse) {
-    const { name = "World" } = request.query;
-    response.send(`Hello ${name}!`);
+    
 }
