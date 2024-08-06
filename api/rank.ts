d5fa127bbd59e036daa4cda2fb073181558fee50
schrobby <schrawby@gmail.com>
2024-08-06 05:15:52 +0200

    Update rank endpoint

--- api/rank.ts
+++ api/rank.ts
@@ -1,17 +1,45 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import ValorantApi from "unofficial-valorant-api";
+import ValorantApi, { APIResponse } from "unofficial-valorant-api";
 
-const vapi = new ValorantApi(process.env.API_KEY);
+const vapi = new ValorantApi(process.env.API_KEY as string);
 
-export default function (request: VercelRequest, response: VercelResponse) {
-  const { name, tag } = request.query;
+console.info(`API Key: ${process.env.API_KEY}`);
 
-  console.info(`API Key: ${process.env.API_KEY}`);
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const { name, tag, puuid } = request.query;
 
-  if (!name || !tag) {
-    response.status(400).send("Name and tag are required");
+  if (!((name && tag) || puuid)) {
+    response
+      .status(400)
+      .send("Missing required parameters (name and tag or puuid)");
     return;
   }
 
-  response.send(`${name}#${tag}`);
+  console.info(`Fetching rank for ${name && tag ? `${name}#${tag}` : puuid}`);
+
+  var { error, data: accountData } = 
+      name && tag
+          ? await vapi.getAccount({ name: name as string, tag: name as string })
+          : await vapi.getAccountByPUUID({ puuid: puuid as string });
+  
+  if (error) {
+    response.status(500).send(error.message);
+    return;
+  }
+  
+  var { error, data: rankData } = await vapi.getMMRByPUUID({ version: "v2", region: accountData?.["region"], puuid: accountData?.["puuid"] });
+
+  if (error) {
+    response.status(500).send(error.message);
+    return;
+  }
+
+  const { current_data: currentData, highest_rank: highestRank } = rankData as any;
+  const message = `${currentData?.["currenttier_patched"]} (${currentData?.["ranking_in_tier"]}rr) [${currentData?.["elo"]} MMR] - \
+    Peak: ${highestRank?.["patched_tier"]} in ${highestRank?.["season"].toUpperCase()} - Last match: ${currentData?.["mmr_change_to_last_game"]}rr`;
+
+  response.send(message);
 }
